b5ee124c76c34bace6c47c6715af64f1ea98a949
Joran Vinicíus Silveira Lage <joranvinicius@gmail.com>
2026-02-22 23:30:15 -0300

    criei struct Cidade e grafo e algumas funções aux

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MAX_CIDADES 100
+#define MAX_NOME 30
 
 typedef struct No {
     int id_destino;
@@ -7,12 +9,45 @@ typedef struct No {
     struct No *prox;
 } No;
 
+typedef struct {
+    char nome[MAX_NOME];
+    No* lista_adj;
+} Cidade;
+
+Cidade grafo[MAX_CIDADES];
+int total_cidades = 0;
+
+int buscar_id(char *nome);
+
 int main (int argc, char *argv[]) {
     int n, conjunto = 1;
     while (scanf("%d", &n) == 1) {
         if (conjunto > 1) {
             printf("\n");
         }
-        
+        printf("Conjunto #%d\n", conjunto++);
+        total_cidades = n;
+
+        for (int i = 0; i < n; i++) {
+            scanf("%s", grafo[i].nome);
+            grafo[i].lista_adj = NULL;
+        }
+
     }
+}
+
+int buscar_id(char *nome) {
+    for (int i = 0; i < total_cidades; i++) {
+        if (strcmp(grafo[i].nome, nome) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+void adicionar_aresta(int origem, int destino, int tempo) {
+    No* novo_no = (No*) malloc(sizeof(No));
+    novo_no->id_destino = destino;
+    novo_no->peso = tempo;
+    novo_no->prox = grafo[origem].lista_adj;
+    grafo[origem].lista_adj = novo_no;
 }
